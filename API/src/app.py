dc2ec1ca6df667b88328f31ab06273760f6da221
Eduardo <86479444+L0G1C06@users.noreply.github.com>
2023-01-24 20:17:16 +0000

    Remove API

--- API/src/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from src.routes.person import router as PersonRouter
-
-app = FastAPI()
-app.include_router(PersonRouter, tags=['Person'], prefix='/person')
-
-@app.get('/test')
-async def root():
-    return {'Hello': "World"}
